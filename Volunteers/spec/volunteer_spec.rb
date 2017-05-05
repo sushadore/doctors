b54681430fa55280adf4ba4943518ec82fd9d854
Susha Dore <susha.dore@gmail.com>
2017-05-05 12:27:35 -0700

    pass 3rd Volunteer class spec .all

--- Volunteers/spec/volunteer_spec.rb
+++ Volunteers/spec/volunteer_spec.rb
@@ -22,4 +22,12 @@ describe Volunteer do
       expect(Volunteer.all).to eq([])
     end
   end
+
+  describe '#==' do
+    it 'is the same volunteer if they have the same name and ID' do
+      volunteer1 = Volunteer.new(:name => 'Sam Bob', :id=> 1
+      volunteer2 = Volunteer.new(:name => 'Sam Bob', :id=> 1
+      expect(volunteer1).to eq(volunteer2)
+    end
+  end
 end
